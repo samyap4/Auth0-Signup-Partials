13e030cbef54895f4c3ac1be8f67159cd74a4532
Sam Yapkowitz <samyapkowitz@gmail.com>
2024-01-18 15:57:39 -0600

    First commit

---
+++ updatePartials.js
@@ -0,0 +1,62 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const prompt = 'signup-id'; 
+const filePath = 'custom sign up fields.html';
+const promptEntryPoint = "form-content-end";
+
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+
+
+const apiUrl = `https://${AUTH0_DOMAIN}/api/v2/prompts/${prompt}/partials`;
+
+const updatePartial = async (filePath) => {
+  const authResponse = await fetch(`https://${AUTH0_DOMAIN}/oauth/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      audience: `https://${AUTH0_DOMAIN}/api/v2/`,
+      grant_type: 'client_credentials',
+    }),
+  });
+
+  const authData = await authResponse.json();
+  console.log(authResponse.status);
+  if (authResponse.status != 200) {
+    console.error(`Failed to authenticate: ${authData}`);
+    return;
+  }
+  var partialData = {}
+  partialData[`${prompt}`] = {};
+  partialData[`${prompt}`][`${promptEntryPoint}`] = fs.readFileSync(filePath, 'utf8');
+
+    console.log(partialData);
+
+  const updateResponse = await fetch(apiUrl, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authData.access_token}`,
+    },
+    body: JSON.stringify(partialData),
+  });
+
+  const updatedPartial = await updateResponse.json();
+  console.log(updatedPartial.status);
+  if (updateResponse.status !== 200) {
+    console.error(`Failed to update partial: ${updatedPartial.error_description}`);
+    return;
+  }
+
+  console.log('partial updated successfully:', updatedPartial);
+};
+
+
+updatePartial(filePath);
